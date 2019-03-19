c15c4ed70ff9b156dc45106ec4601b555d5d119a
samo-war <samo_war@mail.ru>
2019-03-19 03:06:05 +0300

    feat: add functional for getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  const arr = [];
+  preferences.forEach((item, index) => {
+    if (preferences[index] === preferences[preferences[preferences[item - 1] - 1] - 1] &&
+      preferences[preferences[index] - 1] === preferences[item - 1] &&
+      preferences[preferences[preferences[index] - 1] - 1] === preferences[preferences[item - 1] - 1]) {
+        let a = preferences[index];
+        let b = preferences[preferences[index] - 1];
+        let c = preferences[preferences[preferences[index] - 1] - 1];
+        if (a !== b && b !== c && c !== a) {
+          if (arr.indexOf(a) === -1 && arr.indexOf(b) === -1 && arr.indexOf(c) === -1) {
+            arr.push(a, b, c);
+          }
+        }
+    }
+  })
+  return arr.length / 3;
 };
